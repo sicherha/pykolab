7aafe929d98318c7a9bd4a5b6bc04e3c299588bc
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2012-08-08 15:05:01 +0100

    Ship commands to set and list mailbox metadata

---
+++ pykolab/cli/cmd_list_mailbox_metadata.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# Copyright 2010-2012 Kolab Systems AG (http://www.kolabsys.com)
+#
+# Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen a kolabsys.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 or, at your option, any later version
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+
+import commands
+
+import pykolab
+
+from pykolab.imap import IMAP
+from pykolab.translate import _
+from pykolab import utils
+
+log = pykolab.getLogger('pykolab.cli')
+conf = pykolab.getConf()
+
+def __init__():
+    commands.register('list_mailbox_metadata', execute, description=description())
+
+def description():
+    return """Obtain a list of metadata entries on a folder."""
+
+def execute(*args, **kw):
+    try:
+        folder = conf.cli_args.pop(0)
+    except IndexError, errmsg:
+        folder = utils.ask_question(_("Folder name"))
+
+    if len(folder.split('@')) > 1:
+        domain = folder.split('@')[1]
+    else:
+        domain = conf.get('kolab', 'primary_domain')
+
+    imap = IMAP()
+    imap.connect(domain=domain)
+
+    if not imap.has_folder(folder):
+        print >> sys.stderr, _("No such folder %r") % (folder)
+
+    else:
+        metadata = []
+        folders = imap.lm(folder)
+        for folder in folders:
+            print "Folder", folder
+
+            metadata = imap.get_metadata(folder)
+
+            for annotation in metadata[folder].keys():
+                print "  %-49s %s" % (
+                        annotation,
+                        metadata[folder][annotation]
+                    )
