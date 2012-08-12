803935a7e108d3720566a2f141985fea936dc274
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2012-08-12 11:46:59 +0100

    - Add command to list a user's folder subscriptions. Usage:
      $ kolab list-user-subscriptions <user> [<mailbox-pattern>]
    
      Examples:
    
      $ kolab list-user-subscriptions john.doe@example.org
    
      $ kolab list-user-subscriptions \
        john.doe@example.org shared/lists/example.org/memo
    
    Note that a proxyauth login is executed, and mailbox patterns can
    therefore not be suffixed with the usual '@domain' realm idenfifier.

---
+++ pykolab/cli/cmd_list_user_subscriptions.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# Copyright 2010-2012 Kolab Systems AG (http://www.kolabsys.com)
+#
+# Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen a kolabsys.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 or, at your option, any later version
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+
+import commands
+
+import pykolab
+
+from pykolab.imap import IMAP
+from pykolab.translate import _
+from pykolab import utils
+
+log = pykolab.getLogger('pykolab.cli')
+conf = pykolab.getConf()
+
+def __init__():
+    commands.register('list_user_subscriptions', execute, description=description())
+
+def cli_options(*args, **kw):
+    my_option_group = conf.add_cli_parser_option_group(_("CLI Options"))
+    my_option_group.add_option( '--unsubscribed',
+                                dest    = "unsubscribed",
+                                action  = "store_true",
+                                default = False,
+                                help    = _("List unsubscribed folders"))
+
+def description():
+    return _("List the folders a user is subscribed to.")
+
+def execute(*args, **kw):
+    folder_pattern = "*"
+
+    try:
+        user = conf.cli_args.pop(0)
+        try:
+            folder_pattern = conf.cli_args.pop(0)
+        except IndexError, errmsg:
+            pass
+
+    except IndexError, errmsg:
+        user = utils.ask_question(_("User ID"))
+
+    if len(user.split('@')) > 1:
+        domain = user.split('@')[1]
+    else:
+        domain = conf.get('kolab', 'primary_domain')
+
+    imap = IMAP()
+    imap.connect(domain=domain, login=False)
+
+    backend = conf.get(domain, 'imap_backend')
+    if backend == None:
+        backend = conf.get('kolab', 'imap_backend')
+
+    admin_login = conf.get(backend, 'admin_login')
+    admin_password = conf.get(backend, 'admin_password')
+
+    imap.login_plain(admin_login, admin_password, user)
+
+    subscribed_folders = imap.lsub(folder_pattern)
+
+    if conf.unsubscribed:
+        unsubscribed_folders = []
+        all_folders = imap.lm(folder_pattern)
+
+        for folder in all_folders:
+            if not folder in subscribed_folders:
+                unsubscribed_folders.append(folder)
+
+        if len(unsubscribed_folders) > 0:
+            print "\n".join(unsubscribed_folders)
+        else:
+            print _("No unsubscribed folders for user %s") % (user)
+
+    else:
+        print "\n".join(subscribed_folders)
