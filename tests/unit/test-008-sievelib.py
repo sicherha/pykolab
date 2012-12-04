5cce5af0cfed978b8eb4560b761123217c854878
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2012-12-04 15:34:05 +0000

    Add a few additional scripts to test

--- tests/unit/test-008-sievelib.py
+++ tests/unit/test-008-sievelib.py
@@ -2,12 +2,38 @@ import sys
 import unittest
 
 sieve_scripts = [
-        """
+
+# You're average vacation script.
+"""
 require [ "vacation" ];
 
 if anyof (true) {
     vacation :days 1 :subject "Out of Office" "I'm out of the office";
 }
+""",
+
+# A non-any/allof if (control) header (test) structure
+"""
+require ["fileinto"];
+
+if header :contains "X-Spam-Flag" "YES" {
+    fileinto "Spam";
+    stop;
+}
+""",
+
+# The same, all on the same line
+"""
+require ["fileinto"];
+
+if header :contains "X-Spam-Flag" "YES" { fileinto "Spam"; stop; }
+""",
+
+# A little more of a complex list of tests
+"""
+require ["fileinto"];
+
+if allof (header :contains "X-Mailer" "OTRS", header :contains "X-Powered-By" "OTRS", header :contains "Organization" "Example, Inc.") { fileinto "OTRS"; stop; }
 """,
 
     ]
@@ -17,7 +43,7 @@ class TestSievelib(unittest.TestCase):
     def test_001_import_sievelib(self):
         from sievelib.parser import Parser
 
-    def test_002_parse_vacation(self):
+    def test_002_parse_tests(self):
         from sievelib.parser import Parser
         sieve_parser = Parser(debug=True)
 
@@ -26,4 +52,5 @@ class TestSievelib(unittest.TestCase):
             i += 1
             result = sieve_parser.parse(sieve_str)
             if not result:
+                print "Sieve line: %r" % (sieve_parser.lexer.text.split('\n')[(sieve_parser.lexer.text[:sieve_parser.lexer.pos].count('\n'))])
                 raise Exception, "Failed parsing Sieve script #%d: %s" % (i, sieve_parser.error)
