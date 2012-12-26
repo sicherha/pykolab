844c8e9b1a4d21b79222d43f1fbef144f638cfb8
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2012-12-26 14:37:54 +0100

    It is just as easy for purge_users() to call purge_imap()

--- tests/functional/purge_users.py
+++ tests/functional/purge_users.py
@@ -13,25 +13,5 @@ def purge_users():
     for user in users['list']:
         wap_client.user_delete({'user': user})
 
-    auth = Auth()
-    domains = auth.list_domains()
-
-    imap = IMAP()
-    imap.connect()
-
-    folders = []
-
-    for domain,aliases in domains:
-        folders.extend(imap.lm("user/%%@%s" % (domain)))
-
-    for folder in folders:
-        user = folder.replace('user/','')
-
-        recipient = auth.find_recipient(user)
-
-        if len(recipient) == 0 or recipient == []:
-            try:
-                imap.dm(folder)
-            except:
-                pass
-
+    from tests.functional.purge_imap import purge_imap
+    purge_imap()
