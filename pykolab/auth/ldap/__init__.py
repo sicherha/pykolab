dff2be70f952637d0ad0a5941ba5dabdd47b53f3
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2013-01-27 14:36:50 +0000

    Correct the other call to modify ACL entries on IMAP folders from LDAP

--- pykolab/auth/ldap/__init__.py
+++ pykolab/auth/ldap/__init__.py
@@ -1451,8 +1451,7 @@ class LDAP(pykolab.base.Base):
                 not entry['kolabmailfolderaclentry'] == None:
 
             self.imap._set_kolab_mailfolder_acls(
-                    entry['kolabmailfolderaclentry'],
-                    folder=folder_path
+                    entry['kolabmailfolderaclentry']
                 )
 
         #if server == None:
