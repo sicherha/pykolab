0a6d62724aba3145e8bf67e13231bbfbd552ffd8
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2013-04-17 15:00:43 +0200

    Add a 'kolab add-user' command

---
+++ pykolab/cli/cmd_add_user.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright 2010-2012 Kolab Systems AG (http://www.kolabsys.com)
+#
+# Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen a kolabsys.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 or, at your option, any later version
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+
+import commands
+
+import pykolab
+
+from pykolab.translate import _
+
+log = pykolab.getLogger('pykolab.cli')
+conf = pykolab.getConf()
+
+def __init__():
+    commands.register('add_user', execute, description="Add a user.")
+
+def execute(*args, **kw):
+    from pykolab import wap_client
+    # Create the authentication object.
+    # TODO: Binds with superuser credentials!
+    wap_client.authenticate()
+    wap_client.user_add()
+
