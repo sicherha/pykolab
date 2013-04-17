45df636fd79e7f1c909136daef47fbad724b560a
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2013-04-17 15:01:47 +0200

    Add initial set of WAP commands

---
+++ pykolab/cli/wap/cmd_system_capabilities.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Copyright 2010-2012 Kolab Systems AG (http://www.kolabsys.com)
+#
+# Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen a kolabsys.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 or, at your option, any later version
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+
+import sys
+
+import pykolab
+from pykolab.cli import commands
+
+from pykolab.translate import _
+
+log = pykolab.getLogger('pykolab.cli')
+conf = pykolab.getConf()
+
+def __init__():
+    commands.register('system_capabilities', execute, group='wap', description="Display the system capabilities.")
+
+def execute(*args, **kw):
+    from pykolab import wap_client
+    # Create the authentication object.
+    # TODO: Binds with superuser credentials!
+    wap_client.authenticate()
+    system_capabilities = wap_client.system_capabilities()
+
+    if system_capabilities['count'] < 1:
+        print "No system capabilities"
+        sys.exit(1)
+
+    for domain in system_capabilities['list'].keys():
+        print "Domain capabilities for %s" % (domain)
+
+        domain_capabilities = system_capabilities['list'][domain]
+
+        for service in domain_capabilities['actions'].keys():
+            print "  %-15s - %r" % (service, domain_capabilities['actions'][service]['type'])
