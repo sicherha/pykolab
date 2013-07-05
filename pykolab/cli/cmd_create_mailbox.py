cebd042b87b5a85ea6f840b9c51c953dd0b9c282
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2013-07-05 19:26:32 +0100

    Use imap.create_folder() for kolab cm

--- pykolab/cli/cmd_create_mailbox.py
+++ pykolab/cli/cmd_create_mailbox.py
@@ -63,9 +63,7 @@ def execute(*args, **kw):
     imap = IMAP()
     imap.connect()
 
-    admin_login = conf.get('cyrus-imap', 'admin_login')
-
-    imap.cm(mailbox)
+    imap.create_folder(mailbox)
 
     if not conf.metadata == None:
         imap.set_metadata(mailbox, conf.metadata.split('=')[0], conf.metadata.split('=')[1])
