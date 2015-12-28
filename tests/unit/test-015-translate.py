6d68214fb99033bd71297c61495213d2e07984b2
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-12-28 17:05:44 +0100

    Remove functional test from unit test suite

--- tests/unit/test-015-translate.py
+++ tests/unit/test-015-translate.py
@@ -16,15 +16,6 @@ class TestTranslate(unittest.TestCase):
     def test_003_set_lang(self):
         from pykolab.translate import _
         self.assertEqual(_("Folder name"), "Folder name")
-        translate.setUserLanguage('de_DE')
-        self.assertEqual(_("Folder name"), "Ordnername", "German Translation found")
-        translate.setUserLanguage('foo_bar')
-        self.assertEqual(_("Folder name"), "Folder name", "Unknown language falls back to NullTranslations")
-
-    def test_004_unicode(self):
-        from pykolab.translate import _
-        translate.setUserLanguage('de_DE')
-        self.assertEqual(_("recipients: %r"), "Empfänger: %r")
 
 if __name__ == '__main__':
     unittest.main()
