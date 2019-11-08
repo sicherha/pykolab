726512ce5cabc82dc31ca4f8d7e40aa4f55f0028
Aleksander Machniak <machniak@kolabsys.com>
2019-11-08 09:56:14 +0000

    Fix unit tests after fix in utils.parse_ldap_uri()

--- tests/unit/test-009-parse_ldap_uri.py
+++ tests/unit/test-009-parse_ldap_uri.py
@@ -8,9 +8,9 @@ class TestParseLdapUri(unittest.TestCase):
     def test_001_ldap_uri(self):
         ldap_uri = "ldap://localhost"
         result = utils.parse_ldap_uri(ldap_uri)
-        self.assertEqual(result, ("ldap", "localhost", "389", None, None, None, None))
+        self.assertEqual(result, ("ldap", "localhost", "389", None, [None], None, None))
 
     def test_002_ldap_uri_port(self):
         ldap_uri = "ldap://localhost:389"
         result = utils.parse_ldap_uri(ldap_uri)
-        self.assertEqual(result, ("ldap", "localhost", "389", None, None, None, None))
+        self.assertEqual(result, ("ldap", "localhost", "389", None, [None], None, None))
